a4b2cdb53119051d6f6fb2af05b08555c05f7f8a
Eduardo De Souza Santos <119471845+Eduardoss45@users.noreply.github.com>
2024-02-15 21:04:19 -0300

    Adição do fim do quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 // react, componentes, estáticos
 import { useContext, useEffect } from "react";
+import { QuizContext } from "./context/quiz";
 
 import Welcome from "./components/Welcome";
 import Question from "./components/Question";
+import GameOver from "./components/GameOver";
 
 import "./App.css";
-import { QuizContext } from "./context/quiz";
 
 function App() {
   const [quizState, dispatch] = useContext(QuizContext);
@@ -20,6 +21,7 @@ function App() {
       <h1>Quiz de Programação</h1>
       {quizState.gameStage === "Start" && <Welcome />}
       {quizState.gameStage === "Playing" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
     </div>
   );
 }
